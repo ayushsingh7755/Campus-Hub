d742b2849cb7c968242c5b74eb102eee1e574762
Ayush Singh <ayushsingh57571@gmail.com>
2026-07-06 23:25:44 +0530

    vercel added

--- backend/src/App.js
+++ backend/src/App.js
@@ -8,7 +8,7 @@ import { getAuth } from './middleware/auth.middleware.js'
 const app=express()
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173",],
     credentials:true
 }));
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,11 @@
-import 'dotenv/config';
-import  express from 'express'
-import {connectDb} from './database/connect.js'
-import app from './App.js'
-
+import "dotenv/config";
+import { connectDb } from "./database/connect.js";
+import app from "./App.js";
 
 app.get("/", (req, res) => {
-  res.send("Backend working perfectly on 4000");
+  res.send("Backend is running successfully.");
 });
-connectDb().then(()=>{
-    app.listen(process.env.PORT||4000,()=>{
-        console.log(`App is running at the port ${process.env.PORT}`)
-    })}
-)
 
+await connectDb();
 
+export default app;
